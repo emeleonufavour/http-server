f64b1c413e4ffa8042e112eb084e12707efcddd1
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-05-04 11:11:58 +0100

    chore: extracted header from HTTP request

--- app/main.py
+++ app/main.py
@@ -1,5 +1,15 @@
 import socket
 
+def get_header(request: str):
+    """Function to get the headers from an HTTP request"""
+    lines = request.split("\n")
+    header = None
+    for line in lines:
+        if line.startswith("User-Agent:"):
+            header = line.split(":", 1)[1].strip()
+            break
+    return header
+
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
