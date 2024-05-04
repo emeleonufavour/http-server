11c3a8e16a8689d0e0a0009f4367a45cf9edfd3f
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-05-04 11:16:11 +0100

    Implemented concurrent connections :zap:

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import socket
-from header import get_header
-
-
+import sys
+from threading import Thread
+from request import handle_request
 
 def main():
+    fdir = ""
+    if len(sys.argv) == 3 and sys.argv[1] == "--directory":
+        fdir = sys.argv[2]
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server is running on port 4221")
     
@@ -12,12 +15,15 @@ def main():
             print("Waiting for a connection...")
             client_socket, address = server_socket.accept()
             print(f"Connection from {address} has been created")
+            Thread(target=handle_request, args=[client_socket,address,fdir]).start()
 
     except KeyboardInterrupt:
         print("\nServer is shutting down")
     finally:
         server_socket.close()
         print("Server has shut down")
+        
+        
 if __name__ == "__main__":
     main()
     # a = "GET /user-agent HTTP/1.1 Host: localhost:4221 User-Agent: curl/7.64.1 Accept-Encoding: gzip"
